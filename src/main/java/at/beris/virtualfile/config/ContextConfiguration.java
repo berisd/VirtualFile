1c9fc6be333422bbbf1f82adfea79c1d9068f843
Bernd Riedl <bernd.riedl@gmail.com>
2016-05-15 21:34:43 +0200

    Change license to LGPL

--- src/main/java/at/beris/virtualfile/config/ContextConfiguration.java
+++ src/main/java/at/beris/virtualfile/config/ContextConfiguration.java
@@ -3,7 +3,7 @@
  *
  * Copyright 2016 by Bernd Riedl <bernd.riedl@gmail.com>
  *
- * Licensed under GNU General Public License 3.0 or later. 
+ * Licensed under GNU Lesser General Public License 3.0 or later.
  * Some rights reserved. See COPYING, AUTHORS.
  */
 
